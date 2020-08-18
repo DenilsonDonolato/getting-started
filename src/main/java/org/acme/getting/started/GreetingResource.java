655ca02a9c8d6fd2ffb02ec97938f4573d73e277
DenilsonDonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-08-18 12:55:07 -0300

    Alteração para reativo

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -21,38 +21,38 @@ public class GreetingResource {
 
     @POST
     public Uni<Response> login(Login login) {
-        LoginResponse response = new LoginResponse(
+        return Uni.createFrom().item(login)
+            .onItem().transform(l -> {
+                if (l.getUsername() == null || l.getPassword()==null) {
+                    return Response.status(Status.UNAUTHORIZED).build();
+                }
+                LoginResponse response = new LoginResponse(
                 "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTIyNTQyNTcsImF1ZCI6ImdtLWFwcC1hc3Npc3RlZCIsInN1YiI6IjEifQ.Zo6j3Yio5_TYeh45TFjCUoMQe0g3sxVZ82rfwnK7ypw",
                 1, "Fernanda");
-
-        if (login.getUsername() == null || login.getPassword() == null) {
-            return Uni.createFrom().item(() -> Response.status(Status.UNAUTHORIZED).build());
-        }
-
-        return Uni.createFrom().item(() -> Response.ok(response).build());
+                return Response.ok(response).build();
+            });
     }
 
     @POST
     @Path("/{delay}")
     public Uni<Response> loginWithDelay(Login login, @PathParam long delay) {
-        if (delay <= 0) {
-            return Uni.createFrom().item(() -> Response.status(Status.BAD_REQUEST).build());
-        }
-
-        LoginResponse response = new LoginResponse(
+        return Uni.createFrom().item(login).onItem()
+            .transform(t -> {
+                if (delay<=0) {
+                    return Response.status(Status.BAD_REQUEST).build();
+                }
+                try {
+                    Thread.sleep(delay);
+                } catch (Exception e) {
+                    return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+                }
+                if (t.getUsername() == null || t.getPassword() == null) {
+                    return Response.status(Status.UNAUTHORIZED).build();
+                }
+                LoginResponse response = new LoginResponse(
                 "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTIyNTQyNTcsImF1ZCI6ImdtLWFwcC1hc3Npc3RlZCIsInN1YiI6IjEifQ.Zo6j3Yio5_TYeh45TFjCUoMQe0g3sxVZ82rfwnK7ypw",
                 1, "Mariana");
-
-        try {
-            Thread.sleep(delay);
-        } catch (Exception e) {
-            return Uni.createFrom().item(() -> Response.status(Status.INTERNAL_SERVER_ERROR).build());
-        }
-
-        if (login.getUsername() == null || login.getPassword() == null) {
-            return Uni.createFrom().item(() -> Response.status(Status.UNAUTHORIZED).build());
-        }
-
-        return Uni.createFrom().item(() -> Response.ok(response).build());
+                return Response.ok(response).build();
+            });
     }
 }
